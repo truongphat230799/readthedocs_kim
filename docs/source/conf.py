a8709b92a9ccdadf62438d236805e2322965eb56
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2025-12-02 23:27:48 +0700

    test game

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,34 @@
+# -- Project information -----------------------------------------------------
+project = 'Dao Kim Wiki'
+author = 'NGUYEN THI DAO KIM'
+version = '1.0.0'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- HTML theme --------------------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+
+html_logo = 'image/daokim-logo.png'
+html_favicon = 'image/weblogo.png'
+
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/dk_style.css',
+    'css/game.css',
+    'css/custom.css',
+]
 
 html_js_files = [
     'js/xlsx.full.min.js',
@@ -7,8 +37,16 @@ html_js_files = [
     'js/team_picker.js',
 ]
 
-html_css_files = [
-    'css/dk_style.css',
-    'css/game.css',
-    'css/custom.css',   # nếu bạn muốn giữ
-]
+html_theme_options = {
+    'logo_only': True,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'includehidden': True,
+}
+
+html_context = {
+   'display_github': True,
+   'github_user': 'username',
+   'github_repo': 'reponame',
+   'github_version': 'master',
+}
---
+++ docs/source/game.rst
@@ -0,0 +1,41 @@
+Trò chơi lớp học
+=================
+
+Gọi tên học sinh
+-----------------
+
+.. raw:: html
+
+   <div class="game-box">
+     <h2>Gọi tên học sinh</h2>
+     <input type="file" id="studentFile">
+     <button class="game-btn" id="pickStudent">Gọi 1 học sinh</button>
+     <div class="game-result" id="studentResult"></div>
+   </div>
+
+Bốc câu hỏi
+------------
+
+.. raw:: html
+
+   <div class="game-box">
+     <h2>Bốc câu hỏi</h2>
+     <input type="file" id="questionFile">
+     <button class="game-btn" id="pickQuestion">Bốc câu hỏi</button>
+     <div class="game-result" id="questionResult"></div>
+   </div>
+
+Chia đội
+------------
+
+.. raw:: html
+
+   <div class="game-box">
+     <h2>Chia đội</h2>
+     <input type="file" id="teamFile">
+     <br><br>
+     Số đội: <input type="number" id="teamCount" value="2" min="2" max="10">
+     <br><br>
+     <button class="game-btn" id="makeTeam">Chia đội</button>
+     <div class="game-result" id="teamResult"></div>
+   </div>
